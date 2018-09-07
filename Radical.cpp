352453a56cb63cc76df1a355d1661beb5c4853ad
ConejoMoncada <andresm1996@hotmail.com>
2018-09-07 16:28:29 -0600

    sin cout de prueba

--- Radical.cpp
+++ Radical.cpp
@@ -1,6 +1,4 @@
 #include "Radical.h"
-#include<iostream>
-using namespace std;
 Radical::Radical():Real(){
     valor = 0;
     coeficiente = 0;
@@ -12,9 +10,7 @@ Radical::Radical(double pcoeficiente,double pindice,double pradicando):Real(){
     coeficiente = pcoeficiente;
     indice = pindice;
     radicando = pradicando;
-    cout << pow(radicando,1/indice)<< endl;
     valor = coeficiente * pow(radicando,1/indice);
-    cout << valor;
 }
 
 string Radical::toString(){
